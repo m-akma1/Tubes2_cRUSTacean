d313a4167fb33fe163ef9c3b45d18f18e8b4cc4d
Muhammad Akmal <muhammad.akmal.3806@gmail.com>
2026-04-15 13:45:59 +0700

    feat: add basic backend server stub

--- crates/backend/src/main.rs
+++ crates/backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router
+};
+use reqwest::Client;
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new();
+    let app = Router::new()
+    .route("/", get(|| async {"Backend entry point. Tes 1 2 3 aman yak."}))
+    .with_state(client);
+
+    println!("Backend running on http://127.0.0.1:3000");
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
